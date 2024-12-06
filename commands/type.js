bbc318f90880d721cafc3ffd9fb7123b726e9ba3
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2024-12-06 19:39:19 +0200

    Delete

--- commands/type.js
+++ commands/type.js
@@ -4,19 +4,19 @@ CreatePlug({
     command: 'alive',
     category: 'general',
     desc: 'alive',
-    execute: async (message) => {
-        var platform = process.platform; 
-        var runtime = process.version; 
-        var uptime = process.uptime(); 
-        var usage = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2); 
-        var negro = `_*Hy ${message.user}, Im alive*_
+    execute: async (message,conn) => {
+        const platform = process.platform; 
+        const runtime = process.version; 
+        const uptime = process.uptime(); 
+        const usage = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2); 
+        const negro = `_*Hy ${message.user}, Im alive*_
 *_PLATFORM_:* \`${platform}\`
 *_RUNTIME_:* \`Node.js ${runtime}\`
 *_UPTIME_:* \`${Math.floor(uptime / 60)}m ${Math.floor(uptime % 60)}s\`
 *_MEMORY_:* \`${usage}MB\`
 
 _ Type \`alive\` again to refresh the status_`;
-        await message.send(message.user, { negro },{ quoted: message }
+        await conn.send(message.user, { text: negro }
         );
     }
 });
