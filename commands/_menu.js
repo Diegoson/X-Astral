fe2a60b0dfd4cacab4e6e9e494e8e2448176b3ca
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2024-12-08 07:33:29 +0200

    Delete

--- commands/_menu.js
+++ commands/_menu.js
@@ -1,38 +1,42 @@
 const { commands, CreatePlug } = require('../lib/commands');
-const { get_current_date, 
-       monospace 
-      } = require('../lib/index');
+const { monospace } = require('../lib/index');
 const CONFIG = require('../config');
 
 CreatePlug({
     command: 'menu',
     category: 'general',
     desc: 'types',
-    execute: async (message,conn) => {        
+    execute: async (message, conn) => {        
       const gorized = commands.reduce((acc, cmd) => {
           if (!acc[cmd.category]) acc[cmd.category] = [];
-            acc[cmd.category].push(cmd.command);
-            return acc;
-}, {});
-    const namo = () => {
-  return `╭──╼【 ${monospace(CONFIG.app.botname.toUpperCase())} 】\n` +
-`┃ ✦ Prefix  : ${CONFIG.app.prefix}\n` +
-`┃ ✦ User    : ${message.user}\n` +
-`┃ ✦ Date    : ${get_current_date}\n` +
-`┃ ✦ Version : ${CONFIG.app.version}\n` +
-`╰──────────╼`;
-        };
+          acc[cmd.category].push(cmd.command);
+          return acc;
+      }, {});
 
-        const package = (category, cmds) => {
-  return `╭───╼【 *${monospace(category.toUpperCase())}* 】\n` +
-            cmds.map(cmd => `┃ ∘ ${monospace(cmd.toLowerCase())}`).join('\n') + '\n' +
-         `╰──────────╼`;
+      const namo = () => {
+        const now = new Date();
+        const date = now.toLocaleDateString('en-ZA', { timeZone: 'Africa/Johannesburg' });  
+        const time = now.toLocaleTimeString('en-ZA', { timeZone: 'Africa/Johannesburg' });  
+        return `╭──╼【 ${monospace(CONFIG.app.botname.toUpperCase())} 】\n` +
+               `┃ ✦ Prefix  : ${CONFIG.app.prefix}\n` +
+               `┃ ✦ User    : ${message.user}\n` +
+               `┃ ✦ Date    : ${date}\n` +  
+               `┃ ✦ Time    : ${time}\n` +  
+               `┃ ✦ Version : ${CONFIG.app.version}\n` +
+               `╰──────────╼`;
       };
-    let msg = namo() + '\n\n'; 
-        for (const [category, cmds] of Object.entries(gorized)) {
-            msg += package(category, cmds) + '\n\n';
-  }
-      await conn.send(message.user, msg.trim() , {quoted: message});
+
+      const package = (category, cmds) => {
+        return `╭───╼【 *${monospace(category.toUpperCase())}* 】\n` +
+               cmds.map(cmd => `┃ ∘ ```${cmd.toLowerCase())}````).join('\n') + '\n' +
+               `╰──────────╼\nmade with 💘`;
+      };
+
+      let msg = namo() + '\n\n'; 
+      for (const [category, cmds] of Object.entries(gorized)) {
+          msg += package(category, cmds) + '\n\n';
+      }
+      await conn.send(message.user, msg.trim(), {quoted: message});
     }
 });
-  
+               
