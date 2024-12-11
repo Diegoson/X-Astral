8b732256d43555e279231b7ac5b9796545c39a58
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2024-12-11 10:37:38 +0200

    Delete

---
+++ commands/group.js
@@ -0,0 +1,32 @@
+const { CreatePlug } = require('../lib/commands');
+
+CreatePlug({
+    command: 'kick',
+    category: 'group',
+    desc: 'Remove a user from the group',
+    execute: async (message, conn) => {
+        if (!message.isGroup) return;
+        if (!message.isBotAdmin) return message.reply('Um not admin');
+        const participants = await message.groupParticipants;
+        const members = message.mentions;
+        if (message.text === 'all') {
+            const nonAdmins = participants.filter(participant => !participant.admin || participant.admin !== 'admin').map(participant => participant.id);
+            if (nonAdmins.length === 0) return message.reply('_remove_');
+            await message.reply('_Kicking all non-admins.._');
+            for (const user of nonAdmins) {
+                await conn.groupParticipantsUpdate(message.user, [user], 'remove')
+                    .then(() => message.reply(`Removed: ${user}`))
+                    .catch(err => message.reply(`${user}`));
+            }
+        } else if (members.length > 0) {
+            for (const user of members) {
+                await conn.groupParticipantsUpdate(message.user, [user], 'remove')
+                    .then(() => message.reply(`removed: ${user}.`))
+                    .catch(err => message.reply(`${user}.`));
+            }
+        } else {
+            return message.reply('mention_user(s)/type_"all"');
+        }
+    }
+});
+          
