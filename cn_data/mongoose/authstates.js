2f5d90ff3dbeb0091b7d8084b191257bded800b1
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2024-12-12 13:17:17 +0200

    Delete

--- cn_data/mongoose/authstates.js
+++ cn_data/mongoose/authstates.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
+const AuthState = mongoose.model(
+  'AuthState',
+  new mongoose.Schema(
+    {
+      session_id: { type: String, required: true, index: true },
+      data_key: { type: String, required: true, index: true },
+      data_value: { type: String, required: true },
+    },
+    { collection: 'session', timestamps: false }
+  ).index({ session_id: 1, data_key: 1 }, { unique: true })
+);
+
+module.exports = AuthState;
 
