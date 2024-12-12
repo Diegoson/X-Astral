1cb76f22019f6c87a56857431b86059cbbc34dc1
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2024-12-12 17:49:23 +0200

    Delete

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 const dotenv = require('dotenv');
 const path = require('path');
-
-dotenv.config({
-path: path.resolve(__dirname, `${process.env.NODE_ENV || 'development'}.env`)
-});
+dotenv.config({path: path.resolve(__dirname, `${process.env.NODE_ENV || 'development'}.env`)});
 
 const CONFIG = {
     app: {
