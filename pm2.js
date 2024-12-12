15a03f7a0b26bb63b2cf9e5c90a32d4a86af8272
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2024-12-12 16:13:30 +0200

    Delete

---
+++ pm2.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const config = {
+  appName: 'x_astral',
+  scriptPath: './index.js', 
+  instances: 1,
+  execMode: 'fork', 
+  restartDelay: 5000,
+  maxMemory: '1G',
+  watchFiles: true,
+  ignoredDirs: ['node_modules'],
+  enableTimestamps: true,
+};
+
+const PM2Config = () => ({
+  apps: [
+    {
+      name: config.appName,
+      script: config.scriptPath,
+      exec_mode: config.execMode,
+      instances: config.instances,
+      restart_delay: config.restartDelay,
+      watch: config.watchFiles,
+      ignore_watch: config.ignoredDirs,
+      max_memory_restart: config.maxMemory,
+      time: config.enableTimestamps,
+    },
+  ],
+});
+
+module.exports = PM2Config();
+                    
