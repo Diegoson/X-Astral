3440196adf32c62f707265431a4599f0d514082f
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2024-12-16 12:11:29 +0200

    Delete

--- database/getPlugins.js
+++ database/getPlugins.js
@@ -1,28 +1,11 @@
 const mongoose = require("mongoose");
 const pluginSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: String,
-        enum: ['loaded', 'error', 'not_found'],
-        default: 'not_found'
-    },
-    path: {
-        type: String,
-        required: true
-    },
-    error: {
-        type: String,
-        default: null
-    },
-    loadedAt: {
-        type: Date,
-        default: Date.now
-    }
+    name: {type: String,required: true},
+    status: {type: String,enum: ['loaded', 'error', 'not_found'],default: 'not_found'},
+    path: {type: String,required: true},
+    error: {type: String,default: null},
+    loadedAt: {type: Date,default: Date.now}
 });
-
 const Plugin = mongoose.model('Plugin', pluginSchema);
 module.exports = Plugin;
   
