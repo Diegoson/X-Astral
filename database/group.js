f5bf023d89da0ffd487af0f2ca97ee48d6b38b76
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2024-12-16 12:05:46 +0200

    Delete

--- database/group.js
+++ database/group.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 const GroupSchema = new mongoose.Schema({
-    groupId: { type: String, required: true },
+    id: { type: String, required: true },
     welcome: { type: Boolean, default: true },
     welcome: { type: String,default: "*Welcome*: @pushname\n*To*: @gc_name\n*Member*: @number\n*Time*: @time"},
     goodbye: { type: Boolean, default: true },
     goodbye: { type: String,default: "*Goodbye*: @pushname\n*From*: @gc_name\n*Time*: @time\n*Negro dusted*"},
 });
 const Group = mongoose.model("Group", GroupSchema);
-async function settingz(groupId) {
-    let group = await Group.findOne({ groupId });
+async function settingz(id) {
+    let group = await Group.findOne({id});
     if (!group) {
-        group = new Group({ groupId });
+        group = new Group({id});
         await group.save();}
     return group;
 }
