8970d964e582d04d17e63148d96670d2d54bdcc1
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2024-12-20 15:59:29 +0200

    Delete

--- commands/youtube.js
+++ commands/youtube.js
@@ -1,25 +1,25 @@
 const { CreatePlug } = require('../lib/commands');
-const YouTube = require('youtube-sr').default;
+const yts = require('yt-search');
 
 CreatePlug({
-    command: 'yts',
-    category: 'search',
-    desc: 'Search',
-    execute: async (message, conn) => {
-        console.log(message.text.trim().split(' ').slice(1).join(' '));
-        console.log(message.text.trim().split(' '));
-        console.log(message.text.trim());
-        console.log(message.text);
-        const query = message.text;
-        if (!query) return message.reply('(e.g., `yts search term`)');
-        const videos = await YouTube.search(query, { limit: 18, safeSearch: true });
-        if (!videos || videos.length === 0) return message.reply(`No_fund`);
-        const res = videos.map((m, i) => 
-            `*${++i}. ${m.title}*\n` +
-            `Published: ${new Date(m.uploadedAt).toLocaleDateString()}\n` +
-            `https://www.youtube.com/watch?v=${m.id}\n` +
-            `---`
-        ).join("\n");
-        await conn.send(message.user, { text: `*YouTube Search:* _${query}_\n\n${res}` }, { quoted: message });
-    },
+  command: 'yts',
+  category: 'search',
+  desc: 'Search YouTube',
+  execute: async (message, conn, match) => {
+    if (!match || match.trim() === '') return message.reply('_Example: yts funny cat videos_');
+    var args = match.trim();
+    const results = await yts(args);
+    if (!results || !results.videos || results.videos.length === 0) return;
+      const msg = results.videos.slice(0, 18);
+       const res = `*YouTube Search:*` + 
+      msg.filter(video => {
+        const parts = video.timestamp.split(':');
+        const mmm = parts.length === 2 ? parseInt(parts[0]) : parseInt(parts[0]); return mmm >= 18;
+      }).map(({ title, timestamp, views, ago, url }, index) => 
+        `{${index + 1}. ${title}}\n{Duration: ${timestamp}}\n{Views: ${views}}\n{Uploaded: ${ago}}\n{${url}}`
+      ).join('\n\n');
+    if (!res) return;
+       await conn.send(message.user, { text: res }, { quoted: message });
+  },
 });
+        
