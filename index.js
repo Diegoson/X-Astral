25d398222dc1cdd44241637ad119d2c5c563c4d8
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2024-12-21 13:59:57 +0200

    Delete

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ conn.ev.on("messages.upsert", async ({ messages, type }) => {
         }
     });
 
-conn.ev.on("creds.update", saveCreds);       
+conn.ev.on("creds.update", state);       
 conn.ev.on("group-participants.update", async ({ id, participants, action }) => {
     const Settings = await settingz(id); const gcName = (await conn.groupMetadata(id)).subject; const timestamp = new Date().toLocaleString(); 
     for (const participant of participants) { 
