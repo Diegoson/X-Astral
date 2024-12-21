838c44641db42698e0bad7dd7febc43f4b5057ab
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2024-12-21 13:00:09 +0200

    Delete

--- commands/youtube.js
+++ commands/youtube.js
@@ -9,7 +9,7 @@ CreatePlug({
     if (!match) return message.reply('_Example: yts funny cat videos_');
     const results = await YouTube.search(match, { limit: 19 });
     if (!results || results.length === 0) return message.reply('Not_found');
-    let res = '*YouTube Search:*\n';
+    let res = '*YouTube Search:*\n\n';
     for (let i = 0; i < results.length; i++) {
       const video = results[i];
       res += `${i + 1}. *${video.title}*\nDuration: ${video.durationFormatted}\nViews: ${video.views.toLocaleString()}\n${video.url}\n\n`;}
