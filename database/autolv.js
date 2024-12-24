17c949863fca88d80a000d805794ab49fb783463
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2024-12-24 18:07:20 +0200

    Delete

--- database/autolv.js
+++ database/autolv.js
@@ -25,23 +25,25 @@ async function maxUP(message, conn) {
 }
 
 async function detectACTION(update, conn) {
-  const { id, participants } = update;
+  const { id, participants, action } = update; 
   try { for (const participant of participants) {
       const zmg = await conn.profilePictureUrl(participant, 'image').catch(() => null);
       const username = participant.split('@')[0]; 
-      const _msg = `**Promotion Alert**\n**name**: ${username}\nDamn new role`;
-      if (zmg) {
-        await conn.sendMessage(id, {
-          image: { url: zmg },
-          caption: _msg,
-        });
-      } else {
-        await conn.sendMessage(id, { text: promotionMessage }); 
-      }}} catch (error) { console.error(error);
-  }
-}
+      let _msg = '';
+      if (action === 'promote') {
+        _msg = `**Promotion Alert!**\n**Name**: ${username}\nWow new role`;
+      } else if (action === 'demote') {
+        _msg = `**Demotion Alert!** \n**Name**: ${username}\nBeen adjusted`;
+      }  if (_msg) {
+        if (zmg) {
+          await conn.sendMessage(id, {
+            image: { url: zmg },
+            caption: _msg, 
+          });
+        } else {
+          await conn.sendMessage(id, { text: _msg });
+        }}
+    }} catch (error) {console.error(error);
+  }}
 
-       
-
-module.exports = { maxUP, detectACTION, ACTIONdetect };
-    
+module.exports = { maxUP, detectACTION };
