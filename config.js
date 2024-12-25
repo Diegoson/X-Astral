3d98e35f36708603f237adff4e2ac7bb2b7d8fcf
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2024-12-25 13:08:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ const CONFIG = {
         session_name: process.env.SESSION_NAME || '',
         botname: process.env.BOTNAME || 'X-ASTRAL',
         base_url: process.env.BASE_URL || 'https://api.yanzbotz.live/',
-        mongodb: process.env.MONGODB_URL 'mongodb+srv://Xcelsama:Xcel@xcelsama.qpklf.mongodb.net/?retryWrites=true&w=majority&appName=Xcelsama',
+        mongodb: process.env.MONGODB_URL || '',
         version: require('./package.json').version,
         env: process.env.NODE_ENV || 'development',
         prefix: process.env.COMMAND_PREFIX || '.',
         mode: process.env.MODE || 'private',
-        mods: process.env.MODS || '27686881509,27686567257,27828418477,6283899737438'
+        mods: process.env.MODS || '27686881509,27686567257'
     },
     logging: {
         level: process.env.LOG_LEVEL || 'info',
