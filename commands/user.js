8f9ed3736f62394698b81ff703d19d6eae540fbd
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2024-12-27 21:25:04 +0200

    Delete

--- commands/user.js
+++ commands/user.js
@@ -1,5 +1,6 @@
 const { CreatePlug } = require('../lib/commands');
 const translate = require('@vitalets/google-translate-api'); 
+const gTTS = require('gtts');
 
 CreatePlug({
     command: 'whois',
@@ -49,3 +50,19 @@ CreatePlug({
         message.reply(`${result.text}`);
     }
 });
+
+CreatePlug({
+    command: 'tts',
+    category: 'convert',
+    desc: 'speech',
+    execute: async (message, conn, match) => {
+        if (!match) return message.reply('_nned text_');
+        const tts = new gTTS(match, 'en');
+        const path = '/tmp/tts.mp3';
+        tts.save(path, async (err) => {
+            if (err) return message.reply('_err_');
+            await conn.send(message.user, { audio: { url: path }, mimetype: 'audio/mp4' });
+         });
+    }
+});
+    
