3530bd14731a2a26727445ca9cb981cf69493552
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2024-12-29 18:59:59 +0200

    Delete

--- config.js
+++ config.js
@@ -4,19 +4,15 @@ dotenv.config({path: path.resolve(__dirname, `${process.env.NODE_ENV || 'develop
 
 const CONFIG = {
     app: {
-        session_name: process.env.SESSION_NAME || 'Naxor~YTZTUnlqcFQ=',
+        session_name: process.env.SESSION_NAME || '',
         botname: process.env.BOTNAME || 'X-ASTRAL',
-        base_url: process.env.BASE_URL || 'https://api.yanzbotz.live/',
+        base_url: process.env.BASE_URL || '',
         mongodb: process.env.MONGODB_URL || '',
         version: require('./package.json').version,
         env: process.env.NODE_ENV || 'development',
         prefix: process.env.COMMAND_PREFIX || '?',
         mode: process.env.MODE || 'private',
         mods: process.env.MODS || '27686881509,27686567257'
-    },
-    logging: {
-        level: process.env.LOG_LEVEL || 'info',
-        format: process.env.LOG_FORMAT || 'combined'
     },
 };
 
