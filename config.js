7bbaeb27faff0bbc7cfc00f67c6cfe2ca911d665
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-03 13:19:16 +0200

    Delete

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 const path = require('path');
 dotenv.config({path: path.resolve(__dirname, `${process.env.NODE_ENV || 'development'}.env`)});
 const toBool = (x) => (x && (x.toLowerCase() === 'true' || x.toLowerCase() === 'on')) || false;
-const DATABASE_URL = process.env.DATABASE_URL === undefined ? "./database.db" : process.env.DATABASE_URL
+const DATABASE_URL = process.env.DATABASE_URL === undefined ? "./database.db" : process.env.DATABASE_URL;
 
 const CONFIG = {
     app: {
@@ -14,7 +14,8 @@ const CONFIG = {
         env: process.env.NODE_ENV || 'development',
         prefix: process.env.COMMAND_PREFIX || '?',
         mode: process.env.MODE || 'private',
-        mods: process.env.MODS || '27686881509,27686567257'
+        mods: process.env.MODS || '27686881509,27686567257',
+        DATABASE: DATABASE_URL === "./database.db" ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: "postgres", ssl: true, protocol: "postgres", dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
     },
 };
 
