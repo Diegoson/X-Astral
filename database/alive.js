cd0c960510237875b6fae40521ce737496e9d84c
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-03 13:27:00 +0200

    Delete

--- database/alive.js
+++ database/alive.js
@@ -1,26 +1,31 @@
-const mongoose = require('mongoose');
+
+const { DataTypes } = require('sequelize');
 const os = require('os');
 
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true, trim: true },
-    id: { type: String, required: true },
-    platform: { type: String, default: "unknown" },
-    uptime: { type: String, default: "0s" },
-    memoryUsage: { type: String, default: "0MB" },
-    alives: { type: String, default: `Bot Status:\n\nPlatform: {{platform}}\nUptime: {{uptime}}\nMemory Usage: {{memoryUsage}}\n\nIm alive now 💘`},
-    createdAt: { type: Date, default: Date.now },
-    isActive: { type: Boolean, default: true }});
-    userSchema.pre('save', function (next) {
-    this.platform = os.platform();
-    this.uptime = `${Math.floor(process.uptime() / 60)}m ${Math.floor(process.uptime() % 60)}s`;
+const User = sequelize.define('User', {
+    username: { type: DataTypes.STRING, allowNull: false, unique: true, trim: true },
+    id: { type: DataTypes.STRING, allowNull: false },
+    platform: { type: DataTypes.STRING, defaultValue: 'unknown' },
+    uptime: { type: DataTypes.STRING, defaultValue: '0s' },
+    memoryUsage: { type: DataTypes.STRING, defaultValue: '0MB' },
+    alives: { type: DataTypes.STRING, defaultValue: `Bot Status:\n\nPlatform: {{platform}}\nUptime: {{uptime}}\nMemory Usage: {{memoryUsage}}\n\nI'm alive now 💘`,},
+    createdAt: { type: DataTypes.DATE, defaultValue: Sequelize.NOW },
+    isActive: { type: DataTypes.BOOLEAN, defaultValue: true },
+}, { timestamps: false, });
+User.beforeCreate(async (user, options) => {
+    user.platform = os.platform();
+    user.uptime = `${Math.floor(process.uptime() / 60)}m ${Math.floor(process.uptime() % 60)}s`;
     const memoryInMB = (os.totalmem() - os.freemem()) / (1024 * 1024);
-    this.memoryUsage = `${memoryInMB.toFixed(2)}MB`;
-    next();});
-    userSchema.methods.generateAliveMessage = function () {
+    user.memoryUsage = `${memoryInMB.toFixed(2)}MB`;
+});
+
+User.prototype.generateAliveMessage = function () {
     return this.alives
         .replace('{{platform}}', this.platform)
         .replace('{{uptime}}', this.uptime)
         .replace('{{memoryUsage}}', this.memoryUsage);
 };
 
-module.exports = mongoose.model('User', userSchema);
+sequelize.sync();
+module.exports = User;
+        
