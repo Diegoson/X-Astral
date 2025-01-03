55b7702dc7877498dcfcd2ff1d68abf351da0f12
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-03 13:30:24 +0200

    Delete

--- database/group.js
+++ database/group.js
@@ -1,19 +1,13 @@
-const mongoose = require("mongoose");
+const { DataTypes } = require('sequelize');
 
-const GroupSchema = new mongoose.Schema({
-    id: { type: String, required: true },
-    welcome: { type: Boolean, default: true },
-    welcome: { type: String,default: "*Welcome*: @pushname\n*To*: @gc_name\n*Member*: @number\n*Time*: @time"},
-    goodbye: { type: Boolean, default: true },
-    goodbye: { type: String,default: "*Goodbye*: @pushname\n*From*: @gc_name\n*Time*: @time\n*Negro dusted*"},
-});
-const Group = mongoose.model("Group", GroupSchema);
-async function settingz(id) {
-    let group = await Group.findOne({id});
-    if (!group) {
-        group = new Group({id});
-        await group.save();}
-    return group;
+const Group = sequelize.define('Group', {
+    id: { type: DataTypes.STRING, primaryKey: true },
+    welcome: { type: DataTypes.STRING, defaultValue: "*Welcome*: @pushname\n*To*: @gc_name\n*Member*: @number\n*Time*: @time" },
+    goodbye: { type: DataTypes.STRING, defaultValue: "*Goodbye*: @pushname\n*From*: @gc_name\n*Time*: @time\n*Negro dusted*" }
+}, { timestamps: false });
+
+async function groups(id) {
+    return await Group.findOrCreate({ where: { id } });
 }
-module.exports = { settingz };
-        
+
+module.exports = { groups };
